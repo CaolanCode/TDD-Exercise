9b9d7284a20fa79a84a3a7333348a67e2565abd5
CaolanCode <cmacmahon3@gmail.com>
2023-03-31 09:15:32 +0100

    commit capitalise tests

--- tests/capitalise.test.js
+++ tests/capitalise.test.js
@@ -1,5 +1,17 @@
 const capitalise = require('../scripts/capitalise')
 
-test('iReLaNd to Ireland', () => {
-  expect(capitalise('iReLaNd')).toBe('Ireland')
+test('iReLaNd to IReLaNd', () => {
+  expect(capitalise('iReLaNd')).toBe('IReLaNd')
+})
+
+test('ireland to Ireland', () => {
+  expect(capitalise('ireland')).toBe('Ireland')
+})
+
+test('Don\'t capitalise a number', () => {
+  expect(capitalise(1)).toBe('Not a word')
+})
+
+test('Don\'t capitalise multiple words', () => {
+  expect(capitalise("hello there")).toBe('More than one word')
 })
